aaecb3b7a55af0c9388a8606868391a17fc141ed
Richard Nemeth <nemethricsi@gmail.com>
2024-05-29 21:58:09 +0200

    cart icon strokewidth

--- components/cart/cart-icon.tsx
+++ components/cart/cart-icon.tsx
@@ -17,7 +17,7 @@ export default function CartIcon({ empty = true, className = '' }: CartIconProps
         stroke="#1A1E1E"
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth="2"
+        strokeWidth="3"
         d="M9 22a1 1 0 100-2 1 1 0 000 2zM20 22a1 1 0 100-2 1 1 0 000 2zM1 1h4l2.68 13.39a2 2 0 002 1.61h9.72a2 2 0 002-1.61L23 6H6"
       ></path>
       {!empty && (
