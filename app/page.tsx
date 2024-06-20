de655c96bcbcf0bd2d49ba2a9586cf0a4b7e0ffd
Richard Nemeth <nemethricsi@gmail.com>
2024-06-20 17:34:39 +0100

    add content to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import OnePagerNavbar from 'components/layout/navbar/one-pager';
 import Image from 'next/image';
+
+const { COMPANY_NAME, SITE_NAME } = process.env;
 
 export const metadata = {
   description: 'High-performance ecommerce store built with Next.js, Vercel, and Shopify.',
@@ -8,12 +11,43 @@ export const metadata = {
 };
 
 export default async function HomePage() {
+  const currentYear = new Date().getFullYear();
+  const copyrightName = COMPANY_NAME || SITE_NAME || '';
+
   return (
     <>
-      <Image src="/images/BB-pressphoto_3.jpeg" alt="concert" width="1920" height="1080" />
-      <div className="flex justify-center">
-        <Image src="/svg/BB_LOGO.svg" unoptimized alt="logo" width="295" height="251" />
+      <OnePagerNavbar />
+      <div className="relative">
+        <Image src="/images/BB-pressphoto_3.jpeg" alt="concert" width="1920" height="1080" />
+        <div className="absolute inset-0 bg-gradient-to-b from-bb-black via-transparent to-bb-black" />
       </div>
+      <section id="videos" className="h-96 p-8">
+        <h2>VIDEOS</h2>
+      </section>
+      <section id="about" className="p-8">
+        <h2>About</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ipsa obcaecati odio nesciunt
+          placeat quisquam consequuntur ad? Velit reiciendis corrupti fugiat dolorem non doloremque
+          mollitia nihil enim voluptatem vero? Eius.
+        </p>
+      </section>
+      <section id="contact" className="p-8">
+        <h2>Contact</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ipsa obcaecati odio nesciunt
+          placeat quisquam consequuntur ad? Velit reiciendis corrupti fugiat dolorem non doloremque
+          mollitia nihil enim voluptatem vero? Eius.
+        </p>
+      </section>
+      <div className="relative">
+        <Image src="/images/BB-pressphoto_2.jpg" alt="group" width="1920" height="1080" />
+        <div className="absolute inset-0 bg-gradient-to-b from-bb-black via-transparent to-bb-black" />
+      </div>
+      <div className="absolute bottom-0 hidden w-full justify-center p-8 text-sm font-light uppercase md:flex">
+        &copy; {copyrightName} {currentYear}
+      </div>
+      {/* <footer className="flex justify-center p-8 text-white">FOOTER</footer> */}
     </>
   );
 }
