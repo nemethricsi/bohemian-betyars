e1c0c45278e6ba7f077f6cbf7fa6575c5c99e2a9
Richard Nemeth <nemethricsi@gmail.com>
2024-09-04 10:37:19 +0200

    add conatact section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import FacebookIcon from 'components/icons/facebook';
+import FeatherIllu from 'components/icons/feather';
 import InstagramIcon from 'components/icons/instagram';
 import LogoChickenLegIcon from 'components/icons/logo-chicken-leg';
 import SpotifyIcon from 'components/icons/spotify';
 import YouTubeIcon from 'components/icons/youtube';
 import OnePagerNavbar from 'components/layout/navbar/one-pager';
+import SpotifyEmbedPlayer from 'components/spotify-embed-player';
 import VideoPlayer from 'components/video-player';
 import Image from 'next/image';
 
@@ -73,7 +75,7 @@ export default async function HomePage() {
       >
         <h2 className="font-kirakat">Rólunk</h2>
         <div className="flex flex-col items-center gap-6 md:flex-row md:gap-32">
-          <p>
+          <p className="font-light">
             Evés, ivás, duhajkodás, szerelem és sírva vigadás. A Bohemian Betyars 2009-es működése
             óta nem csak ország, de világszerte belopta magát egy tekintélyes méretű közönség
             szívébe speed-folk-freak-punkos, magával ragadó, energikus formációjával. A koncertjeik
@@ -81,7 +83,7 @@ export default async function HomePage() {
             ember zsigerébe hatol, és minden lábat táncra bír.
           </p>
           <LogoChickenLegIcon className="flex-shrink-0 fill-bb-white" />
-          <p>
+          <p className="font-light">
             Kezdetekben igazi csavargók módjára járták körül utcazenélve Európát, mára a hazai és
             európai klubbok és fesztiválok visszajáró vendégei, de megjárták már Kínát, Japánt,
             Dél-Koreát és az Egyesült Államokat is. Legyen utcán, klubban vagy fesztiválon a
@@ -90,13 +92,76 @@ export default async function HomePage() {
           </p>
         </div>
       </section>
-      <section id="contact" className="p-8">
-        <h2>Contact</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ipsa obcaecati odio nesciunt
-          placeat quisquam consequuntur ad? Velit reiciendis corrupti fugiat dolorem non doloremque
-          mollitia nihil enim voluptatem vero? Eius.
-        </p>
+      <section
+        id="contact"
+        className="mx-auto flex max-w-7xl flex-col items-start gap-4 p-8 text-bb-yellow"
+      >
+        <h2 className="font-kirakat">Kontakt</h2>
+        <div className="flex w-full justify-between">
+          <div className="flex flex-col gap-5">
+            <div className="flex flex-col">
+              <p>Koncertszervezés</p>
+              <p className="font-light">Fehér Gábor</p>
+              <a href="tel:+36 70 930 6338" className="font-light">
+                +36 70 930 6338
+              </a>
+            </div>
+            <div className="flex flex-col">
+              <p>Nemzetközi koncertszervezés</p>
+              <p className="font-light">Palágyi Máté</p>
+              <a href="tel:+36 30 574 5787" className="font-light">
+                +36 30 574 5787
+              </a>
+            </div>
+          </div>
+          <div className="flex flex-col items-start justify-between">
+            <a
+              href="https://www.instagram.com/bohemianbetyars"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center justify-center gap-2"
+            >
+              <InstagramIcon className="h-6 w-6 stroke-bb-yellow" />
+              <span className="hidden md:flex">instagram.com/bohemianbetyars</span>
+            </a>
+            <a
+              href="https://www.facebook.com/bohemianbetyars"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center justify-center gap-2"
+            >
+              <FacebookIcon className="h-6 w-6 stroke-bb-yellow" />
+              <span className="hidden md:flex">facebook.com/bohemianbetyars</span>
+            </a>
+            <a
+              href="https://www.youtube.com/@BohemianBetyarsHungary"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center justify-center gap-2"
+            >
+              <YouTubeIcon className="h-6 w-6 stroke-bb-yellow" />
+              <span className="hidden md:flex">youtube.com/bohemianbetyarshungary</span>
+            </a>
+            <a
+              href="https://open.spotify.com/artist/2ezYPSKWBfnFTobN9puCow?si=jxLEJAiPRte-yw2vY1QVwg"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center justify-center gap-2"
+            >
+              <SpotifyIcon className="h-6 w-6 stroke-bb-yellow" />
+              <span className="hidden md:flex">spotify.com/bohemianbetyars</span>
+            </a>
+          </div>
+          <div className="hidden md:flex">
+            <FeatherIllu className="w-40" />
+          </div>
+          <div className="hidden md:flex">
+            <SpotifyEmbedPlayer height={152} />
+          </div>
+        </div>
+        <div className="mt-14 w-full md:hidden">
+          <SpotifyEmbedPlayer />
+        </div>
       </section>
       <div className="relative">
         <Image src="/images/BB-pressphoto_2.jpg" alt="group" width="1920" height="1080" />
