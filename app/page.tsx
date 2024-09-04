634c8cf1cb373a8e0aa71201b950f57989173335
Richard Nemeth <nemethricsi@gmail.com>
2024-09-04 09:31:55 +0200

    add video section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+import FacebookIcon from 'components/icons/facebook';
+import InstagramIcon from 'components/icons/instagram';
+import SpotifyIcon from 'components/icons/spotify';
+import YouTubeIcon from 'components/icons/youtube';
 import OnePagerNavbar from 'components/layout/navbar/one-pager';
+import VideoPlayer from 'components/video-player';
 import Image from 'next/image';
 
 const { COMPANY_NAME, SITE_NAME } = process.env;
@@ -21,8 +26,45 @@ export default async function HomePage() {
         <Image src="/images/BB-pressphoto_3.jpeg" alt="concert" width="1920" height="1080" />
         <div className="absolute inset-0 bg-gradient-to-b from-bb-black via-transparent to-bb-black" />
       </div>
-      <section id="videos" className="h-96 p-8">
-        <h2>VIDEOS</h2>
+      <section id="videos" className="relative z-10 p-8 md:-mt-60">
+        <div className="mb-44 hidden justify-center gap-24 self-stretch border border-pink-500 md:flex">
+          <a
+            href="https://www.instagram.com/bohemianbetyars"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex h-10 w-10 items-center justify-center"
+          >
+            <InstagramIcon className="h-10 w-10 stroke-bb-yellow" />
+          </a>
+          <a
+            href="https://www.facebook.com/bohemianbetyars"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex h-10 w-10 items-center justify-center"
+          >
+            <FacebookIcon className="h-10 w-10 stroke-bb-yellow" />
+          </a>
+          <a
+            href="https://www.youtube.com/@BohemianBetyarsHungary"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex h-10 w-10 items-center justify-center"
+          >
+            <YouTubeIcon className="h-10 w-10 stroke-bb-yellow" />
+          </a>
+          <a
+            href="https://open.spotify.com/artist/2ezYPSKWBfnFTobN9puCow?si=jxLEJAiPRte-yw2vY1QVwg"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex h-10 w-10 items-center justify-center"
+          >
+            <SpotifyIcon className="h-10 w-10 stroke-bb-yellow" lineFill="#1A1E1E" />
+          </a>
+        </div>
+        <h2 className="mb-3 text-center font-kirakat text-lg text-bb-yellow md:text-xl">
+          !!! NEW SONG + VIDEO OUT NOW !!!
+        </h2>
+        <VideoPlayer url="https://www.youtube.com/watch?v=4CLVXnOIWCk" controls light />
       </section>
       <section id="about" className="p-8">
         <h2>About</h2>
