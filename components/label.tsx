368f6ed5c56031c183ef504f196a0eb863d22f28
Richard Nemeth <nemethricsi@gmail.com>
2024-09-09 20:51:04 +0200

    fix line height

--- components/label.tsx
+++ components/label.tsx
@@ -15,7 +15,7 @@ const Label = ({
   return (
     <div className={clsx('absolute bottom-0 left-0 flex w-full px-4 pb-4 @container/label')}>
       <div className="flex items-center rounded-full border bg-white/70 p-1 text-[13px] uppercase text-black backdrop-blur-md">
-        <h3 className="mr-4 line-clamp-2 flex-grow pl-2 leading-none tracking-tight">{title}</h3>
+        <h3 className="mr-4 line-clamp-2 flex-grow pl-2 leading-tight tracking-tight">{title}</h3>
         <Price
           className="flex-none rounded-full bg-bb-black p-2 text-bb-yellow"
           amount={amount}
