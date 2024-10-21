9424037f5c3686322ddcba3e1ed12d6836a473f8
Richard Nemeth <nemethricsi@gmail.com>
2024-10-21 08:27:09 +0200

    add redirect from /cart page

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,11 @@
 import type { NextRequest } from 'next/server';
-// import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return;
-  // return NextResponse.redirect(new URL('/shop', request.url));
+  return NextResponse.redirect(new URL('/shop', request.url));
 }
 
-// export const config = {
-//   matcher: '/'
-// };
+export const config = {
+  matcher: '/cart'
+};
