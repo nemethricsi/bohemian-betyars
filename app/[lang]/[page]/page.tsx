364c7f311a86b907b14a58d6412ce1f4a45774bf
Richard Nemeth <nemethricsi@gmail.com>
2025-02-17 08:51:45 +0100

    move [page] related data under [lang]

--- app/[lang]/[page]/page.tsx
+++ app/[lang]/[page]/page.tsx
@@ -27,6 +27,8 @@ export async function generateMetadata({
 export default async function Page({ params }: { params: { page: string } }) {
   const page = await getPage(params.page);
 
+  console.log({ page });
+
   if (!page) return notFound();
 
   return (
@@ -34,11 +36,14 @@ export default async function Page({ params }: { params: { page: string } }) {
       <h1 className="mb-8 text-5xl font-bold">{page.title}</h1>
       <Prose className="mb-8" html={page.body as string} />
       <p className="text-sm italic">
-        {`This document was last updated on ${new Intl.DateTimeFormat(undefined, {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric'
-        }).format(new Date(page.updatedAt))}.`}
+        {`This document was last updated on ${new Intl.DateTimeFormat(
+          undefined,
+          {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+          }
+        ).format(new Date(page.updatedAt))}.`}
       </p>
     </>
   );
