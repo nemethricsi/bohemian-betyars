db36850ca153c31d0ddbe2b6a666f6871a2a3176
Richard Nemeth <nemethricsi@gmail.com>
2025-02-18 04:24:20 +0100

    make one pager navbar multilang

--- components/layout/navbar/one-pager.tsx
+++ components/layout/navbar/one-pager.tsx
@@ -1,4 +1,4 @@
-import { groq } from 'next-sanity';
+import { defineQuery } from 'next-sanity';
 import Image from 'next/image';
 import { Link } from 'nextjs13-progress';
 import { Suspense } from 'react';
@@ -8,41 +8,48 @@ import LogoBohemianBetyars from 'components/icons/logo-bb';
 import MobileMenu from 'components/layout/navbar/mobile-menu';
 import { Menu } from 'lib/shopify/types';
 
-const menu: Menu[] = [
-  {
-    title: 'Rólunk',
-    path: '#about'
+type MenuItem = { path: string; title: string };
+type Response = {
+  aboutUs: MenuItem;
+  shop: MenuItem;
+  videos: MenuItem;
+  contact: MenuItem;
+};
+
+const query = defineQuery(`*[_id == "menuItems"][0]{
+  aboutUs {
+    path,
+    "title": title[_key == $locale][0].value
   },
-  {
-    title: 'Shop',
-    path: '/shop'
+  shop {
+    path,
+    "title": title[_key == $locale][0].value
   },
-  {
-    title: 'Videók',
-    path: '#videos'
+  videos {
+    path,
+    "title": title[_key == $locale][0].value
   },
-  {
-    title: 'Kapcsolat',
-    path: '#contact'
+  contact {
+    path,
+    "title": title[_key == $locale][0].value
   }
-];
+}`);
 
 export default async function OnePagerNavbar() {
-  const data = await client.fetch(
-    groq`*[_id == "menuItems"][0]{
-    "aboutUs": aboutUs[_key == $locale][0].value,
-    "shop": shop[_key == $locale][0].value,
-    "videos": videos[_key == $locale][0].value,
-    "contact": contact[_key == $locale][0].value
-  }`,
-    { locale: 'hu' }
-  );
+  const data = await client.fetch<Response>(query, { locale: 'hu' });
+
+  const mobileMenu: Menu[] = [
+    { title: data.aboutUs.title, path: data.aboutUs.path },
+    { title: data.shop.title, path: data.shop.path },
+    { title: data.videos.title, path: data.videos.path },
+    { title: data.contact.title, path: data.contact.path }
+  ];
 
   return (
     <>
       <nav className="relative hidden justify-center gap-12  p-8 pt-16 font-kirakat text-xl text-bb-yellow md:flex">
-        <a href="#about">{data.aboutUs}</a>
-        <Link href="/shop">{data.shop}</Link>
+        <a href={data.aboutUs.path}>{data.aboutUs.title}</a>
+        <Link href={data.shop.path}>{data.shop.title}</Link>
         <div className="absolute z-10 flex -translate-x-6 -translate-y-6 justify-center">
           <Image
             src="/svg/BB_LOGO.svg"
@@ -53,13 +60,13 @@ export default async function OnePagerNavbar() {
           />
         </div>
         <div className="w-[295px]" />
-        <a href="#videos">{data.videos}</a>
-        <a href="#contact">{data.contact}</a>
+        <a href={data.videos.path}>{data.videos.title}</a>
+        <a href={data.contact.path}>{data.contact.title}</a>
       </nav>
       <div className="relative block flex-none md:hidden">
         <LogoBohemianBetyars className="absolute left-1/2 top-5 z-10 h-auto w-40 -translate-x-1/2 fill-bb-white md:hidden" />
         <Suspense fallback={null}>
-          <MobileMenu menu={menu} />
+          <MobileMenu menu={mobileMenu} />
         </Suspense>
       </div>
     </>
